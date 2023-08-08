48493546cea0c4ac68ff985413db8496b166e286
Wyatt Ross <2treecko@gmail.com>
2023-08-07 21:31:06 -0400

    jank

--- src/instructions/targets/input_target.rs
+++ src/instructions/targets/input_target.rs
@@ -1,24 +1,38 @@
-use crate::instructions::targets::Target;
+use crate::{instructions::targets::Target, util::raw_data::{self, RawData}, simulator::memory::WordSize};
 
 pub trait InputTarget: Target {
-    fn get(&self) -> Vec<u8>;
+    fn get(&self) -> &RawData;
 }
 
 impl<T: InputTarget> Target for T {}
 
 pub enum InputTargets {
-    ImmediateInput(Vec<u8>),
+    ImmediateInput(RawData),
     LabelReferenceInput(String),
     StringInput(String),
 }
 
+impl InputTargets {
+    fn new_immediate(data: RawData) -> InputTargets {
+        Self::ImmediateInput(data)
+    }
+    
+    fn new_label_reference(data: &String) -> InputTargets {
+        Self::LabelReferenceInput(data.clone())
+    }
+
+    fn new_string(data: &String) -> InputTargets {
+        Self::StringInput(data.clone())
+    }
+}
+
 impl InputTarget for InputTargets {
-    fn get(&self) -> Vec<u8> {
+    fn get(&self) -> &RawData {
         //TODO implement proper getters for the string variants
         match self {
-            InputTargets::ImmediateInput(v) => v.clone(),
-            InputTargets::LabelReferenceInput(s) => Vec::new(),
-            InputTargets::StringInput(s) => Vec::new(),
+            InputTargets::ImmediateInput(v) => v,
+            InputTargets::LabelReferenceInput(s) => &RawData::empty_data(&WordSize::Four),
+            InputTargets::StringInput(s) => &RawData::empty_data(&WordSize::Four),
         }
     }
 }
