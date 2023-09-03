0d18b66872f43b4f9dbace438655f6f387be6037
Trevor Brunette <trevorbrunette@gmail.com>
2023-09-03 15:57:42 -0400

    Added serializable result enum, added js deserialization for it

--- rezasm-web/main.rs
+++ rezasm-web/main.rs
@@ -1,16 +1,22 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+mod util;
+
 extern crate rezasm_core;
 extern crate rezasm_macro;
 extern crate rezasm_app;
 
-use std::sync::{Arc, RwLock, RwLockReadGuard, RwLockWriteGuard};
+use std::sync::{Arc, RwLock, RwLockWriteGuard};
 use lazy_static::lazy_static;
+use serde::{Deserialize, Serialize, Serializer};
 use rezasm_app::instructions::implementation::arithmetic_instructions::register_instructions;
 use rezasm_core::parser::lexer;
+use rezasm_core::simulation::register::Register;
 use rezasm_core::simulation::registry;
 use rezasm_core::simulation::simulator::Simulator;
+use rezasm_core::util::error::EzasmError;
+use crate::util::serial_result::SerialResult;
 
 lazy_static! {
     static ref SIMULATOR: Arc<RwLock<Simulator>> = Arc::new(RwLock::new(Simulator::new()));
@@ -25,10 +31,15 @@ pub fn set_simulator(simulator: Simulator) {
 }
 
 #[tauri::command]
-fn run(line: &str) -> String {
+fn reset() {
+    get_simulator().reset();
+}
+
+#[tauri::command]
+fn load(lines: &str) -> SerialResult<(), String> {
     let mut simulator = get_simulator();
 
-    for line_string in line
+    for line_string in lines
         .lines()
         .map(|string| string.to_string())
         .collect::<Vec<String>>() {
@@ -40,33 +51,60 @@ fn run(line: &str) -> String {
                 Ok(line ) => {
                     match simulator.add_line(line) {
                         Ok(_) => {}
-                        Err(error) => return format!("{:?}", error),
+                        Err(error) => return SerialResult::Err(format!("{:?}", error)),
                     }
                 },
-                Err(error) => return format!("{:?}", error),
+                Err(error) => return SerialResult::Err(format!("{:?}", error)),
             }
         };
     }
-    while !simulator.is_done() || simulator.is_error() {
-        match simulator.run_line_from_pc() {
-            Ok(_) => {}
-            Err(error) => return format!("{:?}", error),
-        }
-    }
-    let return_code: i64 = simulator.get_registers().get_register(&registry::R0.to_string()).unwrap().get_data().int_value();
-    format!("Program completed with exit code {}", return_code)
+    SerialResult::Ok(())
 }
 
 #[tauri::command]
-fn step() -> String {
-    "".to_string()
+fn run() -> SerialResult<(), String> {
+    let mut simulator = get_simulator();
+    while !simulator.is_done() || simulator.is_error() {
+        match simulator.run_line_from_pc() {
+            Ok(_) => {}
+            Err(error) => return SerialResult::Err(format!("{:?}", error)),
+        }
+    }
+    SerialResult::Ok(())
+}
+
+#[tauri::command]
+fn step() -> SerialResult<(), String> {
+    match get_simulator().run_line_from_pc() {
+        Ok(_) => SerialResult::Ok(()),
+        Err(error) => SerialResult::Err(format!("{:?}", error)),
+    }
+}
+
+#[tauri::command]
+fn is_completed() -> bool {
+    get_simulator().is_done() || get_simulator().is_error()
+}
+
+#[tauri::command]
+fn get_exit_status() -> i64 {
+    get_simulator().get_registers().get_register(&registry::R0.to_string()).unwrap().get_data().int_value()
+}
+
+// TODO allow for options to be returned on registered functions
+#[tauri::command]
+fn get_register_value(register: &str) -> Option<i64> {
+    match get_simulator().get_registers().get_register(&register.to_string()) {
+        Ok(x) => Some(x.get_data().int_value()),
+        Err(_) => None,
+    }
 }
 
 fn main() {
     register_instructions();
 
     tauri::Builder::default()
-        .invoke_handler(tauri::generate_handler![run])
+        .invoke_handler(tauri::generate_handler![load, reset, run, step, is_completed, get_exit_status, get_register_value])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
--- src/App.jsx
+++ src/App.jsx
@@ -4,53 +4,173 @@ import ezasmLogo from "./assets/ezasm.svg";
 import { invoke } from "@tauri-apps/api/tauri";
 import "./App.css";
 
+const RESULT_OK = "data";
+const RESULT_ERR = "error";
+
+const isOk = (result) => {
+    // Cast to bool using ! operator, then undo inversion
+    return result[RESULT_OK] || result[RESULT_OK] === null;
+}
+
+const isError = (result) => {
+    return result[RESULT_ERR] || result[RESULT_ERR] === null;
+}
+
+const getOk = (result) => {
+    if (isOk(result)) {
+        return result[RESULT_OK] === null ? {} : result[RESULT_OK];
+    } else {
+        return undefined;
+    }
+}
+
+const getErr = (result) => {
+    if (isError(result)) {
+        return result[RESULT_ERR] === null ? {} : result[RESULT_ERR];
+    } else {
+        return undefined;
+    }
+}
+
+
 function App() {
-  const [result, setResult] = useState("");
-  const [line, setLine] = useState("");
+    const [error, setError] = useState("");
+    const [loaded, setLoaded] = useState(false);
+    const [running, setRunning] = useState(false);
+    const [result, setResult] = useState("");
+    const [lines, setLines] = useState("");
 
-  async function run() {
-    // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
-      setResult(await invoke("run", { line }));
-  }
+    const isErrorState = () => {
+        return error !== "";
+    }
 
-  return (
-    <div className="container">
-      <h1>Welcome to rezasm!</h1>
+    const clearErrorState = () => {
+        setError("");
+    }
 
-      <div className="row">
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo vite" alt="Vite logo" />
-        </a>
-        <a href="https://tauri.app" target="_blank">
-          <img src="/tauri.svg" className="logo tauri" alt="Tauri logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-        <a href="" target="_blank">
-          <img src={ezasmLogo} className="logo react" alt="EzASM logo"></img>
-        </a>
-      </div>
+    const setErrorState = (newState) => {
+        setError(newState);
+        setRunning(false);
+    }
 
-      <form
-        className="row"
-        onSubmit={(e) => {
-          e.preventDefault();
-          run();
-        }}
-      >
-        <textarea
-          id="run-input"
-          cols="32"
-          onChange={(e) => setLine(e.currentTarget.value)}
-          placeholder="Enter some ezasm code..."
-        />
-        <button type="submit">Run!</button>
-      </form>
+    const getErrorState = () => {
+        return error;
+    }
 
-      <p>{result}</p>
-    </div>
-  );
+    async function reset() {
+        await invoke("reset", {});
+        setLoaded(false);
+        clearErrorState();
+        setResult("");
+    }
+
+    async function load() {
+        let result = await invoke("load", {lines});
+        if (isOk(result)) {
+            setLoaded(true);
+        } else {
+            // isErr
+            setLoaded(false);
+            setErrorState(getErr(result));
+        }
+        return result;
+    }
+
+    async function isCompleted() {
+        return await invoke("is_completed", {});
+    }
+
+    async function getExitStatus() {
+        return await invoke("get_exit_status", {});
+    }
+
+    async function run() {
+        await reset();
+        await load();
+        if (!isErrorState()) {
+            // TODO disable run button
+            setRunning(true);
+            await invoke("run", {lines}).then(await (async (runResult) => {
+                // TODO enable run button
+                console.log("Async!!!!");
+                if (isOk(runResult)) {
+                    setResult("Program exited with exit code " +  await getExitStatus());
+                } else {
+                    setErrorState(getErr(runResult));
+                }
+                setRunning(false);
+            }));
+        }
+    }
+
+    async function step() {
+        if (!loaded) {
+            await load();
+            if (!isErrorState()) {
+                setRunning(true);
+            }
+        }
+
+        if (loaded && running && !isErrorState()) {
+            // TODO Disable step button
+            await invoke("step", {}).then(await (async (stepResult) => {
+                // TODO Enable step button
+                setRunning(await isCompleted());
+            }));
+        }
+
+
+    }
+
+    return (
+        <div className="container">
+            <h1>Welcome to rezasm!</h1>
+
+            <div className="row">
+                <a href="https://vitejs.dev" target="_blank">
+                    <img src="/vite.svg" className="logo vite" alt="Vite logo"/>
+                </a>
+                <a href="https://tauri.app" target="_blank">
+                    <img src="/tauri.svg" className="logo tauri" alt="Tauri logo"/>
+                </a>
+                <a href="https://reactjs.org" target="_blank">
+                    <img src={reactLogo} className="logo react" alt="React logo"/>
+                </a>
+                <a href="" target="_blank">
+                    <img src={ezasmLogo} className="logo react" alt="EzASM logo"></img>
+                </a>
+            </div>
+            <div className="row">
+                <textarea
+                    id="run-input"
+                    cols="32"
+                    onChange={(e) => setLines(e.currentTarget.value)}
+                    placeholder="Enter some ezasm code..."
+                />
+                <button onClick={(e) => {
+                    e.preventDefault();
+                    run();
+                }}>
+                    Run
+                </button>
+                <button onClick={(e) => {
+                    e.preventDefault();
+                    step();
+                }}>
+                    Step
+                </button>
+                <button onClick={(e) => {
+                    e.preventDefault();
+                    reset();
+                }}>
+                    Reset
+                </button>
+            </div>
+
+
+            <p>{isErrorState() ? getErrorState() : result}</p>
+        </div>
+    );
 }
 
 export default App;
