efb64263b38482fd861da205793ebc4b32b00d23
Wyatt Ross <2treecko@gmail.com>
2023-09-15 14:07:18 -0400

    Added error type and check for integer division by zero

--- rezasm-app/src/instructions/implementation/arithmetic_instructions.rs
+++ rezasm-app/src/instructions/implementation/arithmetic_instructions.rs
@@ -1,49 +1,54 @@
 use lazy_static::lazy_static;
 use rezasm_core::instructions::instruction_field::InstructionField;
 use rezasm_core::instructions::instruction_registry::register_instruction;
+use rezasm_core::util::error::{EzasmError, SimulatorError};
 use rezasm_macro::instruction;
 
 lazy_static! {
     pub static ref ADD: InstructionField =
         instruction!(add, |simulator: Simulator,
-                           output: InputOutputTarget,
-                           input1: InputTarget,
-                           input2: InputTarget| {
-            let value1 = input1.get(&simulator)?.int_value();
-            let value2 = input2.get(&simulator)?.int_value();
-            let k = value1 + value2;
-            output.set(simulator, RawData::from_int(k, simulator.get_word_size()))
-        });
+                     output: InputOutputTarget,
+                     input1: InputTarget,
+                     input2: InputTarget| {
+                         let value1 = input1.get(&simulator)?.int_value();
+                         let value2 = input2.get(&simulator)?.int_value();
+                         let k = value1 + value2;
+                         output.set(simulator, RawData::from_int(k, simulator.get_word_size()))
+                     });
     pub static ref SUB: InstructionField =
         instruction!(sub, |simulator: Simulator,
-                           output: InputOutputTarget,
-                           input1: InputTarget,
-                           input2: InputTarget| {
-            let value1 = input1.get(&simulator)?.int_value();
-            let value2 = input2.get(&simulator)?.int_value();
-            let k = value1 - value2;
-            output.set(simulator, RawData::from_int(k, simulator.get_word_size()))
-        });
+                     output: InputOutputTarget,
+                     input1: InputTarget,
+                     input2: InputTarget| {
+                         let value1 = input1.get(&simulator)?.int_value();
+                         let value2 = input2.get(&simulator)?.int_value();
+                         let k = value1 - value2;
+                         output.set(simulator, RawData::from_int(k, simulator.get_word_size()))
+                     });
     pub static ref MUL: InstructionField =
         instruction!(mul, |simulator: Simulator,
-                           output: InputOutputTarget,
-                           input1: InputTarget,
-                           input2: InputTarget| {
-            let value1 = input1.get(&simulator)?.int_value();
-            let value2 = input2.get(&simulator)?.int_value();
-            let k = value1 * value2;
-            output.set(simulator, RawData::from_int(k, simulator.get_word_size()))
-        });
+                     output: InputOutputTarget,
+                     input1: InputTarget,
+                     input2: InputTarget| {
+                         let value1 = input1.get(&simulator)?.int_value();
+                         let value2 = input2.get(&simulator)?.int_value();
+                         let k = value1 * value2;
+                         output.set(simulator, RawData::from_int(k, simulator.get_word_size()))
+                     });
     pub static ref DIV: InstructionField =
         instruction!(div, |simulator: Simulator,
-                           output: InputOutputTarget,
-                           input1: InputTarget,
-                           input2: InputTarget| {
-            let value1 = input1.get(&simulator)?.int_value();
-            let value2 = input2.get(&simulator)?.int_value();
-            let k = value1 / value2;
-            output.set(simulator, RawData::from_int(k, simulator.get_word_size()))
-        });
+                     output: InputOutputTarget,
+                     input1: InputTarget,
+                     input2: InputTarget| {
+                         let value1 = input1.get(&simulator)?.int_value();
+                         let value2 = input2.get(&simulator)?.int_value();
+                         if value2 == 0 {
+                             Err(SimulatorError::DivideByZeroError)
+                         } else {
+                             let k = value1 / value2;
+                             output.set(simulator, RawData::from_int(k, simulator.get_word_size()))
+                         }
+                     });
 }
 
 pub fn register_instructions() {
