c9bf67cedb875856d26d6f795280c0e45a8366ad
Nathan Paul <nwpaul2@icloud.com>
2023-10-10 17:32:51 -0400

    Tested all basic arithmetic instructions, float instructions need more testing and bug fixing!

--- rezasm-source/rezasm-core/src/instructions/implementation/arithmetic_instructions.rs
+++ rezasm-source/rezasm-core/src/instructions/implementation/arithmetic_instructions.rs
@@ -93,6 +93,7 @@ lazy_static! {
                            output: InputOutputTarget,
                            input1: InputTarget| {
             let value1 = input1.get(&simulator)?.int_value();
+            // This may not work, requires further testing!
             let k = !value1;
             return output.set(simulator, RawData::from_int(k, simulator.get_word_size()));
         });
@@ -106,7 +107,7 @@ lazy_static! {
             if value2 == 0 {
                 return Err(SimulatorError::DivideByZeroError);
             } else {
-                let k = value1 / value2;
+                let k = value1 % value2;
                 return output.set(simulator, RawData::from_int(k, simulator.get_word_size()));
             }
         });
@@ -117,6 +118,7 @@ lazy_static! {
                            input2: InputTarget| {
             let value1 = input1.get(&simulator)?.int_value();
             let value2 = input2.get(&simulator)?.int_value();
+            // Bug: negative values not accounted for!
             let k = value1 << value2;
             return output.set(simulator, RawData::from_int(k, simulator.get_word_size()));
         });
@@ -127,38 +129,35 @@ lazy_static! {
                            input2: InputTarget| {
             let value1 = input1.get(&simulator)?.int_value();
             let value2 = input2.get(&simulator)?.int_value();
+            // Bug: negative values not accounted for!
             let k = value1 >> value2;
             return output.set(simulator, RawData::from_int(k, simulator.get_word_size()));
         });
     pub static ref INC: Instruction =
         instruction!(inc, |simulator: Simulator,
-                           output: InputOutputTarget,
-                           input1: InputTarget| {
-            let value1 = input1.get(&simulator)?.int_value();
+                           output: InputOutputTarget| {
+            let value1 = output.get(&simulator)?.int_value();
             let k = value1 + 1;
             return output.set(simulator, RawData::from_int(k, simulator.get_word_size()));
         });
     pub static ref DEC: Instruction =
         instruction!(dec, |simulator: Simulator,
-                           output: InputOutputTarget,
-                           input1: InputTarget| {
-            let value1 = input1.get(&simulator)?.int_value();
+                           output: InputOutputTarget| {
+            let value1 = output.get(&simulator)?.int_value();
             let k = value1 - 1;
             return output.set(simulator, RawData::from_int(k, simulator.get_word_size()));
         });
     pub static ref DECF: Instruction =
         instruction!(decf, |simulator: Simulator,
-                           output: InputOutputTarget,
-                           input1: InputTarget| {
-            let value1 = input1.get(&simulator)?.float_value();
+                           output: InputOutputTarget| {
+            let value1 = output.get(&simulator)?.float_value();
             let k = value1 - 1.0;
             return output.set(simulator, RawData::from_float(k, simulator.get_word_size()));
         });
     pub static ref INCF: Instruction =
         instruction!(incf, |simulator: Simulator,
-                           output: InputOutputTarget,
-                           input1: InputTarget| {
-            let value1 = input1.get(&simulator)?.float_value();
+                           output: InputOutputTarget| {
+            let value1 = output.get(&simulator)?.float_value();
             let k = value1 + 1.0;
             return output.set(simulator, RawData::from_float(k, simulator.get_word_size()));
         });
@@ -254,7 +253,8 @@ lazy_static! {
                            input1: InputTarget| {
             let value1 = input1.get(&simulator)?.float_value();
             
-            let k = value1 as i64;
+            // This still bugs, does not account properly for the 0 case!
+            let k = f64::ceil(value1 - 1.0f64) as i64;
 
             return output.set(simulator, RawData::from_int(k, simulator.get_word_size()));
         });
