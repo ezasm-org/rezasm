e787cbbb74ba387dbd16d371b82226f7e85d7477
Trevor Brunette <trevorbrunette@gmail.com>
2023-10-20 19:10:47 -0400

    Cargo fmt

--- rezasm-source/rezasm-core/src/instructions/implementation/branch_instructions.rs
+++ rezasm-source/rezasm-core/src/instructions/implementation/branch_instructions.rs
@@ -12,103 +12,103 @@ use crate::instructions::targets::input_target::InputTarget;
 use crate::instructions::targets::output_target::Output;
 
 lazy_static! {
-  pub static ref BEQ: Instruction = 
-    instruction!(beq, |simulator: Simulator,
-                       input1: InputTarget,
-                       input2: InputTarget,
-                       label: InputTarget| {
-      let pc_num = registry::get_register_number(&registry::PC.to_string()).unwrap();
-      let output = InputOutputTarget::RegisterInputOutput(pc_num);
-      let value1 = input1.get(&simulator)?.int_value();
-      let value2 = input2.get(&simulator)?.int_value();
-      if value1 == value2 {
-        return output.set(simulator, label.get(&simulator).unwrap());
-      } else {
-        return Ok(());
-      }
-    });
-  pub static ref BNE: Instruction = 
-    instruction!(bne, |simulator: Simulator,
-                       input1: InputTarget,
-                       input2: InputTarget,
-                       label: InputTarget| {
-      let pc_num = registry::get_register_number(&registry::PC.to_string()).unwrap();
-      let output = InputOutputTarget::RegisterInputOutput(pc_num);
-      let value1 = input1.get(&simulator)?.int_value();
-      let value2 = input2.get(&simulator)?.int_value();
-      if value1 != value2 {
-        return output.set(simulator, label.get(&simulator).unwrap());
-      } else {
-        return Ok(());
-      }
-    });
-  pub static ref BLT: Instruction =
-    instruction!(blt, |simulator: Simulator,
-                       input1: InputTarget,
-                       input2: InputTarget,
-                       label: InputTarget| {
-      let pc_num = registry::get_register_number(&registry::PC.to_string()).unwrap(); 
-      let output = InputOutputTarget::RegisterInputOutput(pc_num);
-      let value1 = input1.get(&simulator)?.int_value();
-      let value2 = input2.get(&simulator)?.int_value();
-      if value1 < value2 {
-        return output.set(simulator, label.get(&simulator).unwrap());
-      } else {
-        return Ok(());
-      }
-    });
-  pub static ref BLE: Instruction =
-    instruction!(ble, |simulator: Simulator,
-                        input1: InputTarget,
-                        input2: InputTarget,
-                        label: InputTarget| {
-      let pc_num = registry::get_register_number(&registry::PC.to_string()).unwrap(); 
-      let output = InputOutputTarget::RegisterInputOutput(pc_num);
-      let value1 = input1.get(&simulator)?.int_value();
-      let value2 = input2.get(&simulator)?.int_value();
-      if value1 <= value2 {
-        return output.set(simulator, label.get(&simulator).unwrap());
-      } else {
-        return Ok(());
-      }
-    });
-  pub static ref BGT: Instruction =
-    instruction!(bgt, |simulator: Simulator,
-                        input1: InputTarget,
-                        input2: InputTarget,
-                        label: InputTarget| {
-      let pc_num = registry::get_register_number(&registry::PC.to_string()).unwrap(); 
-      let output = InputOutputTarget::RegisterInputOutput(pc_num);
-      let value1 = input1.get(&simulator)?.int_value();
-      let value2 = input2.get(&simulator)?.int_value();
-      if value1 > value2 {
-        return output.set(simulator, label.get(&simulator).unwrap());
-      } else {
-        return Ok(());
-      }
-    });
-  pub static ref BGE: Instruction =
-    instruction!(bge, |simulator: Simulator,
-                        input1: InputTarget,
-                        input2: InputTarget,
-                        label: InputTarget| {
-      let pc_num = registry::get_register_number(&registry::PC.to_string()).unwrap(); 
-      let output = InputOutputTarget::RegisterInputOutput(pc_num);
-      let value1 = input1.get(&simulator)?.int_value();
-      let value2 = input2.get(&simulator)?.int_value();
-      if value1 >= value2 {
-        return output.set(simulator, label.get(&simulator).unwrap());
-      } else {
-        return Ok(());
-      }
-    });
+    pub static ref BEQ: Instruction =
+        instruction!(beq, |simulator: Simulator,
+                           input1: InputTarget,
+                           input2: InputTarget,
+                           label: InputTarget| {
+            let pc_num = registry::get_register_number(&registry::PC.to_string()).unwrap();
+            let output = InputOutputTarget::RegisterInputOutput(pc_num);
+            let value1 = input1.get(&simulator)?.int_value();
+            let value2 = input2.get(&simulator)?.int_value();
+            if value1 == value2 {
+                return output.set(simulator, label.get(&simulator).unwrap());
+            } else {
+                return Ok(());
+            }
+        });
+    pub static ref BNE: Instruction =
+        instruction!(bne, |simulator: Simulator,
+                           input1: InputTarget,
+                           input2: InputTarget,
+                           label: InputTarget| {
+            let pc_num = registry::get_register_number(&registry::PC.to_string()).unwrap();
+            let output = InputOutputTarget::RegisterInputOutput(pc_num);
+            let value1 = input1.get(&simulator)?.int_value();
+            let value2 = input2.get(&simulator)?.int_value();
+            if value1 != value2 {
+                return output.set(simulator, label.get(&simulator).unwrap());
+            } else {
+                return Ok(());
+            }
+        });
+    pub static ref BLT: Instruction =
+        instruction!(blt, |simulator: Simulator,
+                           input1: InputTarget,
+                           input2: InputTarget,
+                           label: InputTarget| {
+            let pc_num = registry::get_register_number(&registry::PC.to_string()).unwrap();
+            let output = InputOutputTarget::RegisterInputOutput(pc_num);
+            let value1 = input1.get(&simulator)?.int_value();
+            let value2 = input2.get(&simulator)?.int_value();
+            if value1 < value2 {
+                return output.set(simulator, label.get(&simulator).unwrap());
+            } else {
+                return Ok(());
+            }
+        });
+    pub static ref BLE: Instruction =
+        instruction!(ble, |simulator: Simulator,
+                           input1: InputTarget,
+                           input2: InputTarget,
+                           label: InputTarget| {
+            let pc_num = registry::get_register_number(&registry::PC.to_string()).unwrap();
+            let output = InputOutputTarget::RegisterInputOutput(pc_num);
+            let value1 = input1.get(&simulator)?.int_value();
+            let value2 = input2.get(&simulator)?.int_value();
+            if value1 <= value2 {
+                return output.set(simulator, label.get(&simulator).unwrap());
+            } else {
+                return Ok(());
+            }
+        });
+    pub static ref BGT: Instruction =
+        instruction!(bgt, |simulator: Simulator,
+                           input1: InputTarget,
+                           input2: InputTarget,
+                           label: InputTarget| {
+            let pc_num = registry::get_register_number(&registry::PC.to_string()).unwrap();
+            let output = InputOutputTarget::RegisterInputOutput(pc_num);
+            let value1 = input1.get(&simulator)?.int_value();
+            let value2 = input2.get(&simulator)?.int_value();
+            if value1 > value2 {
+                return output.set(simulator, label.get(&simulator).unwrap());
+            } else {
+                return Ok(());
+            }
+        });
+    pub static ref BGE: Instruction =
+        instruction!(bge, |simulator: Simulator,
+                           input1: InputTarget,
+                           input2: InputTarget,
+                           label: InputTarget| {
+            let pc_num = registry::get_register_number(&registry::PC.to_string()).unwrap();
+            let output = InputOutputTarget::RegisterInputOutput(pc_num);
+            let value1 = input1.get(&simulator)?.int_value();
+            let value2 = input2.get(&simulator)?.int_value();
+            if value1 >= value2 {
+                return output.set(simulator, label.get(&simulator).unwrap());
+            } else {
+                return Ok(());
+            }
+        });
 }
 
 pub fn register_instructions() {
-  register_instruction(&BEQ);
-  register_instruction(&BNE);
-  register_instruction(&BLT);
-  register_instruction(&BLE);
-  register_instruction(&BGT);
-  register_instruction(&BGE);
+    register_instruction(&BEQ);
+    register_instruction(&BNE);
+    register_instruction(&BLT);
+    register_instruction(&BLE);
+    register_instruction(&BGT);
+    register_instruction(&BGE);
 }
