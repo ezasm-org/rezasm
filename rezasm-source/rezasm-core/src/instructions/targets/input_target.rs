5a34749c9c93ce2b2f74f6b3ce99a3f9db7d6e2a
Trevor Brunette <trevorbrunette@gmail.com>
2023-10-24 17:36:18 -0400

    Fix invalid labels not erroring on get call (#51)

--- rezasm-source/rezasm-core/src/instructions/targets/input_target.rs
+++ rezasm-source/rezasm-core/src/instructions/targets/input_target.rs
@@ -74,9 +74,10 @@ impl Input for InputTarget {
     fn get(&self, simulator: &Simulator) -> Result<RawData, SimulatorError> {
         match self {
             InputTarget::ImmediateInput(x) => Ok(x.clone()),
-            InputTarget::LabelReferenceInput(s) => simulator
-                .get_label_line_number(s)
-                .map(|x| RawData::from_int(x.clone(), simulator.get_word_size())),
+            InputTarget::LabelReferenceInput(s) => {
+                let line = simulator.get_label_line_number(s)?;
+                Ok(RawData::from_int(line, simulator.get_word_size()))
+            },
             InputTarget::StringInput(s) => simulator
                 .get_memory()
                 .get_string_immediate_address(s)
