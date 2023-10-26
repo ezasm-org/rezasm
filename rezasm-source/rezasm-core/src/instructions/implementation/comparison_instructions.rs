7fcbf0eeb7ac9927e5eaf4a43e166eb1d028aa2d
Trevor Brunette <trevorbrunette@gmail.com>
2023-10-26 11:06:05 -0400

    Cargo fmt

--- rezasm-source/rezasm-core/src/instructions/implementation/comparison_instructions.rs
+++ rezasm-source/rezasm-core/src/instructions/implementation/comparison_instructions.rs
@@ -11,91 +11,91 @@ use crate::instructions::targets::output_target::Output;
 use crate::util::raw_data::RawData;
 
 lazy_static! {
-  pub static ref SEQ: Instruction =
-    instruction!(seq, |simulator: Simulator,
-                       output: InputOutputTarget,
-                       input1: InputTarget,
-                       input2: InputTarget| {
-      let value1 = input1.get(&simulator)?.int_value();
-      let value2 = input2.get(&simulator)?.int_value();
-      if value1 == value2 {
-        return output.set(simulator, RawData::from_int(1, simulator.get_word_size()));
-      } else {
-        return output.set(simulator, RawData::from_int(0, simulator.get_word_size()));
-      }
-    });
-  pub static ref SNE: Instruction =
-    instruction!(sne, |simulator: Simulator,
-                        output: InputOutputTarget,
-                        input1: InputTarget,
-                        input2: InputTarget| {
-      let value1 = input1.get(&simulator)?.int_value();
-      let value2 = input2.get(&simulator)?.int_value();
-      if value1 != value2 {
-        return output.set(simulator, RawData::from_int(1, simulator.get_word_size()));
-      } else {
-        return output.set(simulator, RawData::from_int(0, simulator.get_word_size()));
-      }
-    });
-  pub static ref SLT: Instruction =
-    instruction!(slt, |simulator: Simulator,
-                        output: InputOutputTarget,
-                        input1: InputTarget,
-                        input2: InputTarget| {
-      let value1 = input1.get(&simulator)?.int_value();
-      let value2 = input2.get(&simulator)?.int_value();
-      if value1 < value2 {
-        return output.set(simulator, RawData::from_int(1, simulator.get_word_size()));
-      } else {
-        return output.set(simulator, RawData::from_int(0, simulator.get_word_size()));
-      }
-    });
-  pub static ref SLE: Instruction =
-    instruction!(sle, |simulator: Simulator,
-                        output: InputOutputTarget,
-                        input1: InputTarget,
-                        input2: InputTarget| {
-      let value1 = input1.get(&simulator)?.int_value();
-      let value2 = input2.get(&simulator)?.int_value();
-      if value1 <= value2 {
-        return output.set(simulator, RawData::from_int(1, simulator.get_word_size()));
-      } else {
-        return output.set(simulator, RawData::from_int(0, simulator.get_word_size()));
-      }
-    });
-  pub static ref SGT: Instruction =
-    instruction!(sgt, |simulator: Simulator,
-                        output: InputOutputTarget,
-                        input1: InputTarget,
-                        input2: InputTarget| {
-      let value1 = input1.get(&simulator)?.int_value();
-      let value2 = input2.get(&simulator)?.int_value();
-      if value1 > value2 {
-        return output.set(simulator, RawData::from_int(1, simulator.get_word_size()));
-      } else {
-        return output.set(simulator, RawData::from_int(0, simulator.get_word_size()));
-      }
-    });
-  pub static ref SGE: Instruction =
-    instruction!(sge, |simulator: Simulator,
-                        output: InputOutputTarget,
-                        input1: InputTarget,
-                        input2: InputTarget| {
-      let value1 = input1.get(&simulator)?.int_value();
-      let value2 = input2.get(&simulator)?.int_value();
-      if value1 >= value2 {
-        return output.set(simulator, RawData::from_int(1, simulator.get_word_size()));
-      } else {
-        return output.set(simulator, RawData::from_int(0, simulator.get_word_size()));
-      }
-    });
+    pub static ref SEQ: Instruction =
+        instruction!(seq, |simulator: Simulator,
+                           output: InputOutputTarget,
+                           input1: InputTarget,
+                           input2: InputTarget| {
+            let value1 = input1.get(&simulator)?.int_value();
+            let value2 = input2.get(&simulator)?.int_value();
+            if value1 == value2 {
+                return output.set(simulator, RawData::from_int(1, simulator.get_word_size()));
+            } else {
+                return output.set(simulator, RawData::from_int(0, simulator.get_word_size()));
+            }
+        });
+    pub static ref SNE: Instruction =
+        instruction!(sne, |simulator: Simulator,
+                           output: InputOutputTarget,
+                           input1: InputTarget,
+                           input2: InputTarget| {
+            let value1 = input1.get(&simulator)?.int_value();
+            let value2 = input2.get(&simulator)?.int_value();
+            if value1 != value2 {
+                return output.set(simulator, RawData::from_int(1, simulator.get_word_size()));
+            } else {
+                return output.set(simulator, RawData::from_int(0, simulator.get_word_size()));
+            }
+        });
+    pub static ref SLT: Instruction =
+        instruction!(slt, |simulator: Simulator,
+                           output: InputOutputTarget,
+                           input1: InputTarget,
+                           input2: InputTarget| {
+            let value1 = input1.get(&simulator)?.int_value();
+            let value2 = input2.get(&simulator)?.int_value();
+            if value1 < value2 {
+                return output.set(simulator, RawData::from_int(1, simulator.get_word_size()));
+            } else {
+                return output.set(simulator, RawData::from_int(0, simulator.get_word_size()));
+            }
+        });
+    pub static ref SLE: Instruction =
+        instruction!(sle, |simulator: Simulator,
+                           output: InputOutputTarget,
+                           input1: InputTarget,
+                           input2: InputTarget| {
+            let value1 = input1.get(&simulator)?.int_value();
+            let value2 = input2.get(&simulator)?.int_value();
+            if value1 <= value2 {
+                return output.set(simulator, RawData::from_int(1, simulator.get_word_size()));
+            } else {
+                return output.set(simulator, RawData::from_int(0, simulator.get_word_size()));
+            }
+        });
+    pub static ref SGT: Instruction =
+        instruction!(sgt, |simulator: Simulator,
+                           output: InputOutputTarget,
+                           input1: InputTarget,
+                           input2: InputTarget| {
+            let value1 = input1.get(&simulator)?.int_value();
+            let value2 = input2.get(&simulator)?.int_value();
+            if value1 > value2 {
+                return output.set(simulator, RawData::from_int(1, simulator.get_word_size()));
+            } else {
+                return output.set(simulator, RawData::from_int(0, simulator.get_word_size()));
+            }
+        });
+    pub static ref SGE: Instruction =
+        instruction!(sge, |simulator: Simulator,
+                           output: InputOutputTarget,
+                           input1: InputTarget,
+                           input2: InputTarget| {
+            let value1 = input1.get(&simulator)?.int_value();
+            let value2 = input2.get(&simulator)?.int_value();
+            if value1 >= value2 {
+                return output.set(simulator, RawData::from_int(1, simulator.get_word_size()));
+            } else {
+                return output.set(simulator, RawData::from_int(0, simulator.get_word_size()));
+            }
+        });
 }
 
 pub fn register_instructions() {
-  register_instruction(&SEQ);
-  register_instruction(&SNE);
-  register_instruction(&SLT);
-  register_instruction(&SLE);
-  register_instruction(&SGT);
-  register_instruction(&SGE);
+    register_instruction(&SEQ);
+    register_instruction(&SNE);
+    register_instruction(&SLT);
+    register_instruction(&SLE);
+    register_instruction(&SGT);
+    register_instruction(&SGE);
 }
