f9b111f5ad25137a530947fcb17913f4869ca435
michaelni678 <michael.ni678@gmail.com>
2023-11-03 16:59:50 -0400

    readi, readf, readc, reads is wip

--- rezasm-source/rezasm-core/src/instructions/implementation/terminal_instructions.rs
+++ rezasm-source/rezasm-core/src/instructions/implementation/terminal_instructions.rs
@@ -2,7 +2,7 @@ use std::io;
 
 use lazy_static::lazy_static;
 
-use crate::{instructions::{instruction::Instruction, targets::input_output_target::InputOutputTarget, instruction_registry::register_instruction}, instruction, util::{raw_data::RawData, error::{SimulatorError, IoError, ParserError}}};
+use crate::{instructions::{instruction::Instruction, targets::{input_output_target::InputOutputTarget, input_target::Input}, instruction_registry::register_instruction}, instruction, util::{raw_data::RawData, error::{IoError, ParserError}}};
 use crate::instructions::targets::output_target::Output;
 
 
@@ -31,10 +31,30 @@ lazy_static! {
             let k: char = buf.trim().parse().map_err(ParserError::from)?;
             return output.set(simulator, RawData::from_char(k));
         });
+    pub static ref READS: Instruction =
+        instruction!(reads, |simulator: Simulator,
+                            input1: InputOutputTarget,
+                            input2: InputOutputTarget| {
+            let mut address = input1.get(simulator)?.int_value();
+            let max_size = input2.get(simulator)?.int_value();
+            let mut buf = String::new();
+            io::stdin().read_line(&mut buf).map_err(IoError::from)?;
+            let mut s: String = buf.trim().parse().unwrap();
+            s.truncate(max_size as usize);
+            println!("{} {} {}", address, s, max_size);
+            for c in s.chars() {
+                simulator.get_memory_mut().write(address as usize, &RawData::from_char(c))?;
+                address += simulator.get_memory().word_size().value() as i64;
+            }
+            simulator.get_memory_mut().write(address as usize, &RawData::from_char('\0'))?;
+            Ok(())
+        });
+
 }
 
 pub fn register_instructions() {
     register_instruction(&READI);
     register_instruction(&READF);
     register_instruction(&READC);
+    register_instruction(&READS);
 }
