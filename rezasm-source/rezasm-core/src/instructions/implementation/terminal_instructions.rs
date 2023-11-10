44599efa82fc3cf4d220d4edde274963b6340b53
michaelni678 <michael.ni678@gmail.com>
2023-11-10 16:38:54 -0500

    readln

--- rezasm-source/rezasm-core/src/instructions/implementation/terminal_instructions.rs
+++ rezasm-source/rezasm-core/src/instructions/implementation/terminal_instructions.rs
@@ -56,21 +56,31 @@ lazy_static! {
         instruction!(readln, |simulator: Simulator,
                             input1: InputOutputTarget,
                             input2: InputOutputTarget| {
-            /*
-            For some reason this scanner crate has *type*_until() for every type instead of just taking
-            a closure that returns a bool for comparing??? So there isn't really a simple way to get both
-            use next_bytes() and read_line().
-
             let mut address = input1.get(simulator)?.int_value();
             let max_size = input2.get(simulator)?.int_value();
-            let s = simulator.terminal_stream().unwrap().next_bytes(max_size as usize).map_err(IoError::from)?.unwrap();
-            let s = String::from_utf8(s).unwrap();
+            let stream = simulator.terminal_stream().unwrap();
+            let mut s = String::new();
+            // Read until new line or max size reached.
+            let mut count = 0;
+            while let Some(next) = stream.next_bytes(1).map_err(IoError::from)? {
+                let n = std::str::from_utf8(&next).unwrap().trim();
+                // If newline, break out.
+                if n == "\n" {
+                    break;
+                }
+                // Add the token.
+                s.push_str(n);
+                // Increment the count. If the maximimum size is reached, break out.
+                count += 1;
+                if count == max_size {
+                    break;
+                }
+            }
             for c in s.chars() {
                 simulator.get_memory_mut().write(address as usize, &RawData::from_char(c))?;
                 address += simulator.get_memory().word_size().value() as i64;
             }
             simulator.get_memory_mut().write(address as usize, &RawData::from_char('\0'))?;
-            */
             Ok(())
         });
     pub static ref READLN2: Instruction =
