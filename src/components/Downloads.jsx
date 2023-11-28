7c2d1464cc09bf3131875ce4cb5b78ebd5c5efe2
Trevor Brunette <trevorbrunette@gmail.com>
2023-11-28 16:38:55 -0500

    Added Routing (#80)
    * Added routing

---
+++ src/components/Downloads.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Downloads() {
+    return (
+        <div>
+            <p>Downloads</p>
+        </div>
+    );
+}
+
+export default Downloads;
---
+++ src/components/Home.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import {useNavigate} from "react-router-dom";
+import {CODE_PATH} from "../App.jsx";
+
+function Home() {
+
+    const navigate = useNavigate();
+
+    return (
+        <div>
+            <p>Home</p>
+            <button onClick={() => navigate(CODE_PATH)}>Take me to the code</button>
+        </div>
+    );
+}
+
+export default Home;
