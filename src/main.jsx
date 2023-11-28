81d50b04871b23cc8aff94ec26c7d5e8b83d4bf6
Trevor Brunette <trevorbrunette@gmail.com>
2023-11-28 13:15:45 -0500

    Removed extra newline

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,6 @@ import ReactDOM from "react-dom/client";
 import "../dist/output.css";
 import App from "./App.jsx";
 
-
 ReactDOM.createRoot(document.getElementById("root")).render(
     <React.StrictMode>
         <App />
