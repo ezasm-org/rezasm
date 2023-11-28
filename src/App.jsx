d5b217166a731af5e1e4b1e29bd0c27557df369b
Trevor Brunette <trevorbrunette@gmail.com>
2023-11-28 16:45:16 -0500

    Added Router (#82)
    * Added routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,55 +1,26 @@
-import React, {useEffect, useRef, useState} from "react";
-import RegistryView from "./components/RegistryView.jsx";
-import {loadWasm} from "./rust_functions.js";
-
+import React from "react";
+import {HashRouter, Route, Routes} from "react-router-dom";
+import Code from "./components/Code.jsx";
+import Home from "./components/Home.jsx";
+import Downloads from "./components/Downloads.jsx";
 import "../dist/output.css";
-import MemoryView from "./components/MemoryView.jsx";
-import Console from "./components/Console.jsx";
-import Controls from "./components/Controls.jsx";
-import Editor from "./components/Editor.jsx";
-import {useSimulator} from "./components/simulator.js";
+
+const HOME_PATH = "/";
+const CODE_PATH = "/code/";
+const DOWNLOAD_PATH = "/downloads/";
 
 function App() {
-
-    const {
-        state,
-        error,
-        exitCode,
-        setState,
-        setCode,
-        setInstructionDelay,
-        registerCallback,
-        start,
-        stop,
-        step,
-        load,
-        reset
-    } = useSimulator();
-    const [wasmLoaded, setWasmLoaded] = useState(false);
-
-    useEffect(() => {
-        loadWasm()
-            .then((loaded) => setWasmLoaded(loaded))
-            .catch(() => setWasmLoaded(false));
-    }, []);
-
     return (
-        <div className="container">
-            <div className="fill">
-                <Controls state={state} setState={setState} start={start} stop={stop} step={step} reset={reset} load={load} error={error}/>
-                <div className="mt-2 mb-2 row codearea">
-                    <Editor state={state} setCode={setCode} />
-                    <RegistryView loaded={wasmLoaded} registerCallback={registerCallback} />
-                </div>
-            </div>
-            <div className="fill">
-                <MemoryView loaded={wasmLoaded} registerCallback={registerCallback} />
-            </div>
-            <div className="fill">
-                <Console loaded={wasmLoaded} registerCallback={registerCallback} exitCode={exitCode} error={error} />
-            </div>
-        </div>
+        <HashRouter future={{ v7_startTransition: true }}>
+            <Routes>
+                <Route path={HOME_PATH} element={<Home />} />
+                <Route path={CODE_PATH} element={<Code />} />
+                <Route path={DOWNLOAD_PATH} element={<Downloads />} />
+            </Routes>
+        </HashRouter>
     );
 }
 
 export default App;
+
+export { HOME_PATH, CODE_PATH, DOWNLOAD_PATH };
