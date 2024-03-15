c77ed7b53d327bacbcca306ad6b6da6b5e48bbe5
Aleks Bekker <alekso.bekker@gmail.com>
2024-03-15 17:16:18 -0400

    Fix string-based readers

--- rezasm-source/rezasm-core/src/instructions/implementation/terminal_input_instructions.rs
+++ rezasm-source/rezasm-core/src/instructions/implementation/terminal_input_instructions.rs
@@ -1,8 +1,4 @@
-use crate::simulation::simulator::Simulator;
-use crate::simulation::transform::transformable::{self, Transformable};
-use crate::simulation::transform::transformation::{self, Transformation};
-use crate::util::error::IoError;
-use crate::util::word_size::{self, WordSize};
+use crate::simulation::transform::transformable::Transformable;
 use crate::{
     instruction,
     instructions::{
@@ -22,14 +18,16 @@ lazy_static! {
     pub static ref READI: Instruction =
         instruction!(readi, |simulator: Simulator, output: InputOutputTarget| {
             let mut scanner = ScannerAscii::new(simulator.get_reader_mut());
-            let word_size = simulator.get_word_size();
 
-            let (transformable, data) = match scanner.next_i64()? {
-                Some(int) => (Transformable::InputOutputTransformable(output), RawData::from_int(int, word_size)),
-                None => (Transformable::NullOpTransformable, RawData::empty_data(word_size)),
+            let Some(num) = scanner.next_i64()? else {
+                return Ok(TransformationSequence::new_nullop(simulator)?);
             };
 
-            let transformation = transformable.create_transformation(simulator, data)?;
+            let word_size = simulator.get_word_size();
+
+            let data = RawData::from_int(num, word_size);
+            let transformation = Transformable::InputOutputTransformable(output)
+                .create_transformation(simulator, data)?;
 
             Ok(TransformationSequence::new_single(transformation))
         });
@@ -37,14 +35,15 @@ lazy_static! {
     pub static ref READF: Instruction =
         instruction!(readf, |simulator: Simulator, output: InputOutputTarget| {
             let mut scanner = ScannerAscii::new(simulator.get_reader_mut());
-            let word_size = simulator.get_word_size();
 
-            let (transformable, data) = match scanner.next_f64()? {
-                Some(float) => (Transformable::InputOutputTransformable(output), RawData::from_float(float, word_size)),
-                None => (Transformable::NullOpTransformable, RawData::empty_data(word_size)),
+            let Some(num) = scanner.next_f64()? else {
+                return Ok(TransformationSequence::new_nullop(simulator)?);
             };
 
-            let transformation = transformable.create_transformation(simulator, data)?;
+            let word_size = simulator.get_word_size();
+
+            let data = RawData::from_float(num, word_size);
+            let transformation = Transformable::InputOutputTransformable(output).create_transformation(simulator, data)?;
 
             Ok(TransformationSequence::new_single(transformation))
         });
@@ -52,16 +51,15 @@ lazy_static! {
     pub static ref READC: Instruction =
         instruction!(readc, |simulator: Simulator, output: InputOutputTarget| {
             let mut scanner = ScannerAscii::new(simulator.get_reader_mut());
-            let word_size = simulator.get_word_size();
 
-            // FIX: for some reason the type system hates this line
-            // FIX: the word size here should be one, but I don't know what function to use for this
-            let (transformable, data): (Transformable, RawData) = match scanner.next_char()? {
-                Some(char) => (Transformable::InputOutputTransformable(output), RawData::(char as i64, word_size)),
-                None => (Transformable::NullOpTransformable, RawData::empty_data(word_size)),
+            let Some(ch) = scanner.next_char()? else {
+                return Ok(TransformationSequence::new_nullop(simulator)?);
             };
 
-            let transformation = transformable.create_transformation(simulator, data)?;
+            let word_size = simulator.get_word_size();
+
+            let data = RawData::from_int(ch as i64, word_size);
+            let transformation = Transformable::InputOutputTransformable(output).create_transformation(simulator, data)?;
 
             Ok(TransformationSequence::new_single(transformation))
         });
@@ -69,31 +67,22 @@ lazy_static! {
     pub static ref READS: Instruction = instruction!(
         reads,
         |simulator: Simulator, input1: InputOutputTarget, input2: InputOutputTarget| {
-            let mut scanner = ScannerAscii::new(simulator.get_reader_mut());
-            let word_size = simulator.get_word_size();
 
-            // Get all relevant address stuff as integers for math
-            let word_bytes = word_size.value();
-            let target = input1.get(simulator)?.int_value() as usize;
             let len = input2.get(simulator)?.int_value() as usize;
+            let mut scanner = ScannerAscii::new(simulator.get_reader_mut());
 
-            let string = scanner.next_bytes(len)?;
-
-            // Return NullOp if input is whitespace
-            let Some(string) = string else {
-                return TransformationSequence::new_nullop(simulator);
+            let Some(mut bytes) = scanner.next_bytes(len)? else {
+                return Ok(TransformationSequence::new_nullop(simulator)?);
             };
+            bytes.push(b'\0');
 
-            let transformation_vec = string.iter().enumerate()
-                // FIX: This skips all invalid transformations, which may not be the desired behavior
-                .filter_map(|(offset, _)| {
-                    let transformable = Transformable::MemoryTransformable(target + offset * word_bytes);
-                    let data = RawData::new(&[string[offset]]);
-                    Some(transformable.create_transformation(simulator, data).ok()?)
-                })
-                .collect::<Vec<_>>();
+            let address = input1.get(simulator)?.int_value() as usize;
+            let word_size = simulator.get_word_size().value();
 
-            Ok(TransformationSequence::new(transformation_vec))
+            let transformation = Transformable::MemoryTransformable(bytes.len())
+                .create_transformation(simulator, RawData::new(&bytes))?;
+
+            Ok(TransformationSequence::new_single(transformation))
         }
     );
 
@@ -101,28 +90,55 @@ lazy_static! {
         instruction!(reads, |simulator: Simulator, input1: InputOutputTarget| {
             let mut scanner = ScannerAscii::new(simulator.get_reader_mut());
 
-            let bytes = scanner.next_raw()?.ok_or(IoError::ReadError)?;
+            let Some(mut bytes) = scanner.next()? else {
+                return Ok(TransformationSequence::new_nullop(simulator)?);
+            };
+            bytes.push('\0');
 
             let address = input1.get(simulator)?.int_value() as usize;
             let word_size = simulator.get_word_size().value();
-            bytes.into_iter().enumerate()
-                .map(|(offset, byte)| (address + offset * word_size, byte))
-                .map(|(address, byte)| Transformation::new(output, output.value, byte));
-            // FIX: need correct way to get output and output value for Transformation
 
-            Ok(TransformationSequence::new_empty())
+            let transformation = Transformable::MemoryTransformable(bytes.len())
+                .create_transformation(simulator, RawData::new(bytes.as_bytes()))?;
+
+            Ok(TransformationSequence::new_single(transformation))
         });
 
     pub static ref READLN: Instruction = instruction!(
         readln,
         |simulator: Simulator, input1: InputOutputTarget, input2: InputOutputTarget| {
-            todo!();
+            let len = input2.get(simulator)?.int_value() as usize;
+            let mut scanner = ScannerAscii::new(simulator.get_reader_mut());
+
+            let Some(bytes) = scanner.next_line()? else {
+                return Ok(TransformationSequence::new_nullop(simulator)?);
+            };
+
+            let address = input1.get(simulator)?.int_value() as usize;
+            let word_size = simulator.get_word_size().value();
+
+            let transformation = Transformable::MemoryTransformable(bytes.len())
+                .create_transformation(simulator, RawData::new(&bytes.as_bytes()[0..len]))?;
+
+            Ok(TransformationSequence::new_single(transformation))
         }
     );
 
     pub static ref READLN_UNSIZED: Instruction =
         instruction!(readln, |simulator: Simulator, input1: InputOutputTarget| {
-            todo!();
+            let mut scanner = ScannerAscii::new(simulator.get_reader_mut());
+
+            let Some(bytes) = scanner.next_line()? else {
+                return Ok(TransformationSequence::new_nullop(simulator)?);
+            };
+
+            let address = input1.get(simulator)?.int_value() as usize;
+            let word_size = simulator.get_word_size().value();
+
+            let transformation = Transformable::MemoryTransformable(bytes.len())
+                .create_transformation(simulator, RawData::new(bytes.as_bytes()))?;
+
+            Ok(TransformationSequence::new_single(transformation))
         });
 }
 
