5e89d7c155a5ae5477a62b00f58aa7187de25dd4
Wyatt Ross <2treecko@gmail.com>
2024-04-15 15:34:32 -0400

    added reader tests

--- tests/common/reader.rs
+++ tests/common/reader.rs
@@ -1,30 +1,31 @@
-use std::collections::VecDeque;
 use std::io::{Read, Write};
+use std::collections::VecDeque;
 
-use rezasm_core::{simulation::reader::Reader, util::as_any::AsAny};
+use rezasm_core::simulation::reader::Reader;
+use rezasm_core::util::as_any::AsAny;
 
 #[derive(Debug)]
-pub struct TauriReader {
+pub struct TestReader {
     buffer: VecDeque<u8>,
 }
 
-impl TauriReader {
-    pub fn new() -> TauriReader {
-        TauriReader {
-            buffer: VecDeque::new(),
+impl TestReader {
+    pub fn new() -> TestReader {
+        TestReader {
+            buffer: VecDeque::new()
         }
     }
 }
 
-impl Reader for TauriReader {}
+impl Reader for TestReader {}
 
-impl Read for TauriReader {
+impl Read for TestReader {
     fn read(&mut self, buf: &mut [u8]) -> std::io::Result<usize> {
         self.buffer.read(buf)
     }
 }
 
-impl Write for TauriReader {
+impl Write for TestReader {
     fn write(&mut self, buf: &[u8]) -> std::io::Result<usize> {
         self.buffer.write(buf)
     }
@@ -34,8 +35,7 @@ impl Write for TauriReader {
     }
 }
 
-
-impl AsAny for TauriReader {
+impl AsAny for TestReader {
     fn as_any(&self) -> &dyn std::any::Any {
         self
     }
