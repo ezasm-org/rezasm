2f72d39532a915a16411a5fea1e38c5500e7d607
Aleks Bekker <alekso.bekker@gmail.com>
2024-07-19 14:55:13 -0400

    Add iterative Fibonacci test
    Created a test for a Fibonacci implementation that uses the `jump`
    command to iteratively compute a Fibonacci number.

--- rezasm-source/rezasm-core/src/lib.rs
+++ rezasm-source/rezasm-core/src/lib.rs
@@ -16,3 +16,45 @@ pub mod util;
 #[cfg(test)]
 pub mod test_utils;
 
+#[cfg(test)]
+mod tests {
+    use std::{fs, io};
+
+    use parser::lexer::parse_lines;
+    use simulation::{program::Program, registry, simulator::Simulator};
+    use util::word_size::WordSize;
+
+    use super::*;
+
+    #[test]
+    fn test_fibo() {
+        let word_size = WordSize::Eight;
+        let file_name = "MainFile".to_string();
+
+        let mut simulator = Simulator::new_custom(&word_size, 1024, Box::new(io::stdout()));
+
+        let path = format!("{}/example/fibonacci_jump.ez", env!("CARGO_MANIFEST_DIR"));
+        let code = fs::read_to_string(path).expect("File read failed");
+        let lines = parse_lines(&code, &word_size).expect("Lexing failed");
+        let mut program = Program::new();
+        lines.into_iter().for_each(|line| {
+            program
+                .add_line(line, file_name.clone())
+                .expect("Failed to add line")
+        });
+
+        while !simulator.is_done() {
+            simulator.run_line_from_pc().expect("Line panicked");
+        }
+
+        assert_eq!(
+            simulator
+                .get_registers_mut()
+                .get_register(&registry::A1.to_string())
+                .expect("Register access error")
+                .get_data()
+                .int_value(),
+            89i64
+        );
+    }
+}
